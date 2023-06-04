7c8900b07f258f828df15a5399c886319bcee13d
Patrycja Dziabala <patrycja.dziabala@gmail.com>
2023-06-04 11:48:43 +0200

    Initial Commit

---
+++ BlueberryLove/BlueberryLoveApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlueberryLoveApp.swift
+//  BlueberryLove
+//
+//  Created by Patka on 04/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlueberryLoveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
